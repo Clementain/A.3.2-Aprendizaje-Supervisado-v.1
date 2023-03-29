1010b61b7075ef0d40d619451b6fd2c2f541f4db
Clementain <clentecarmona@gmail.com>
2023-03-28 22:44:46 -0600

    Ya funciona, se hicieron unas modificaciones y se adjunta el excell con los datos usados

--- A.3.2 Aprendizaje Supervisado v.1.py
+++ A.3.2 Aprendizaje Supervisado v.1.py
@@ -1,11 +1,15 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 
-df = pd.DataFrame({'enfermedad': np.repeat(np.array(['Alergia', 'Infección de garganta', 'Resfriado']), (36, 27, 38)),
-                   'sintomas': np.repeat(np.array(['Fiebre', 'DolorGarganta', 'Congestion', 'DolorCabeza']),
-                                         (46, 48, 61, 44))})
+# Crear dataframe con datos de enfermedades y síntomas
+df = pd.DataFrame({
+    'Enfermedad': ['Alergia', 'Alergia', 'Alergia', 'Alergia', 'Infección de garganta', 'Infección de garganta', 'Infección de garganta', 'Infección de garganta', 'Resfriado', 'Resfriado', 'Resfriado', 'Resfriado'],
+    'Síntoma': ['Fiebre', 'Dolor de garganta', 'Congestión', 'Dolor de cabeza', 'Fiebre', 'Dolor de garganta', 'Congestión', 'Dolor de cabeza', 'Fiebre', 'Dolor de garganta', 'Congestión', 'Dolor de cabeza'],
+    'Casos': [19, 17, 19, 16, 14, 11, 15, 12, 13, 20, 27, 16]
+})
 
-datos = pd.crosstab(index=df['enfermedad'],
-                    columns=df['sintomas'], margins=True)
-
-print(df)
+tabla_contingencia = pd.crosstab(
+    df.Enfermedad, df.Síntoma, values=df.Casos, aggfunc='sum')
+tabla_contingencia.loc['Total enfermedad'] = tabla_contingencia.sum()
+tabla_contingencia['Total síntoma'] = tabla_contingencia.sum(axis=1)
+print(tabla_contingencia)
