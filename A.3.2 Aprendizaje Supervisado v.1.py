0e776662608881f4afe78a4a4728fcae068d6b01
Clementain <clentecarmona@gmail.com>
2023-03-28 22:52:54 -0600

    Corrección errores menores

--- A.3.2 Aprendizaje Supervisado v.1.py
+++ A.3.2 Aprendizaje Supervisado v.1.py
@@ -1,15 +1,12 @@
 import pandas as pd
 import numpy as np
 
-# Crear dataframe con datos de enfermedades y síntomas
 df = pd.DataFrame({
-    'Enfermedad': ['Alergia', 'Alergia', 'Alergia', 'Alergia', 'Infección de garganta', 'Infección de garganta', 'Infección de garganta', 'Infección de garganta', 'Resfriado', 'Resfriado', 'Resfriado', 'Resfriado'],
-    'Síntoma': ['Fiebre', 'Dolor de garganta', 'Congestión', 'Dolor de cabeza', 'Fiebre', 'Dolor de garganta', 'Congestión', 'Dolor de cabeza', 'Fiebre', 'Dolor de garganta', 'Congestión', 'Dolor de cabeza'],
+    'Enfermedad': np.repeat(['Alergia', 'Infección de garganta', 'Resfriado'], [4, 4, 4]),
+    'Síntoma': np.tile(['Fiebre', 'Dolor de garganta', 'Congestión', 'Dolor de cabeza'], 3),
     'Casos': [19, 17, 19, 16, 14, 11, 15, 12, 13, 20, 27, 16]
 })
 
 tabla_contingencia = pd.crosstab(
-    df.Enfermedad, df.Síntoma, values=df.Casos, aggfunc='sum')
-tabla_contingencia.loc['Total enfermedad'] = tabla_contingencia.sum()
-tabla_contingencia['Total síntoma'] = tabla_contingencia.sum(axis=1)
+    df.Enfermedad, df.Síntoma, values=df.Casos, aggfunc='sum', margins=True)
 print(tabla_contingencia)
