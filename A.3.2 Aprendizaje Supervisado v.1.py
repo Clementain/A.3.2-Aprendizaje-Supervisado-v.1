094bb60b8e3d2aa044aeb784c0723ce0cce4a477
Ruslan <s19120135@alumnos.itsur.edu.mx>
2023-03-31 12:18:11 -0600

    Se agregaron las probabilidades

--- A.3.2 Aprendizaje Supervisado v.1.py
+++ A.3.2 Aprendizaje Supervisado v.1.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import numpy as np
 
+probAlergia = 0.0
+probInfeccion = 0.0
+proResfriado = 0.0
+congestion = 0
+dolorCabeza = 0
+dolroGarganta = 0
+fiebre = 0
+
 df = pd.DataFrame({
     'Enfermedad': np.repeat(['Alergia', 'Infección de garganta', 'Resfriado'], [4, 4, 4]),
     'Síntoma': np.tile(['Fiebre', 'Dolor de garganta', 'Congestión', 'Dolor de cabeza'], 3),
@@ -10,3 +18,56 @@ df = pd.DataFrame({
 tabla_contingencia = pd.crosstab(
     df.Enfermedad, df.Síntoma, values=df.Casos, aggfunc='sum', margins=True)
 print(tabla_contingencia)
+
+def evaluarSintomas(condicion):
+    if condicion == "alergia":
+        row = 0
+    elif condicion == "infeccion":
+        row = 1
+    elif condicion == "resfriado":
+        row = 2
+
+    if congestion == 1:
+        sintoma1 = tabla_contingencia.iloc[row, 0] / tabla_contingencia.iloc[3, 0]
+    else:
+        sintoma1 = 0
+
+    if dolorCabeza == 1:
+        sintoma2 = tabla_contingencia.iloc[row, 1] / tabla_contingencia.iloc[3, 1]
+    else:
+        sintoma2 = 0
+
+    if dolroGarganta == 1:
+        sintoma3 = tabla_contingencia.iloc[row, 2] / tabla_contingencia.iloc[3, 2]
+    else:
+        sintoma3 = 0
+
+    if fiebre == 1:
+        sintoma4 = tabla_contingencia.iloc[row, 3] / tabla_contingencia.iloc[3, 3]
+    else:
+        sintoma4 = 0
+
+    return (sintoma1 + sintoma2 + sintoma3 + sintoma4)
+    #return (sintoma1 + sintoma2 + sintoma3 + sintoma4) / tabla_contingencia.ioc[row, 4]
+
+
+while True:
+    print("<'n--------- BIENVENIDO AL CONSULTORIO ---------")
+    print("\nPor favor indique si presenta algun sintoma '1' para indicar que SI y '0' para indicar que NO")
+
+    congestion = int(input("¿Tienes Congestion? -> "))
+    dolorCabeza = int(input("¿Tienes Dolor de Cabeza? -> "))
+    dolroGarganta = int(input("¿Tienes Dolor de Garganta? -> "))
+    fiebre = int(input("¿Tienes Congestion? -> "))
+    print("\n")
+
+    probAlergia =  round(evaluarSintomas("alergia"), 4)
+    probInfeccion =  round(evaluarSintomas("infeccion"), 4)
+    proResfriado = round(evaluarSintomas("resfriado"), 4)
+
+    print("USTED PUEDE TENER....")
+    print("Alergia: " + str(probAlergia))
+    print("Infeccion de garganta: " + str(probInfeccion))
+    print("Resfriado: " +str(proResfriado))
+
+    input("ESPERA ")
